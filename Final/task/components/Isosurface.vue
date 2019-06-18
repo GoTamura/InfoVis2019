68c95cee43d9a46ef81dba1b997651ed27a10f5d
Go Tamura <1655078t@stu.kobe-u.ac.jp>
2019-06-18 12:07:58 +0900

    headのscriptの実行順が不定でバグる

--- Final/task/components/Isosurface.vue
+++ Final/task/components/Isosurface.vue
@@ -8,14 +8,14 @@ export default {
   head() {
     return {
       script: [
-        { src: 'three.min.js' },
-        { src: 'TrackballControls.js' },
+        { src: '/InfoVis2019/Final/task/dist/three.min.js' },
+        { src: '/InfoVis2019/Final/task/dist/TrackballControls.js' },
         { src: 'https://naohisas.github.io/KVS.js/Build/KVS.min.js' },
         { src: 'https://naohisas.github.io/KVS.js/Build/KVS2THREE.js' },
         { src: 'https://naohisas.github.io/KVS.js/Source/KVSLobsterData.js' },
-        { src: 'Isosurfaces.js' },
-        { src: 'Bounds.js' },
-        { src: 'w09_main_ex01.js' }
+        { src: '/InfoVis2019/Final/task/dist/Isosurfaces.js' },
+        { src: '/InfoVis2019/Final/task/dist/Bounds.js' },
+        { src: '/InfoVis2019/Final/task/dist/w09_main_ex01.js' }
       ]
     }
   },
